735638b70895a97bc614bb1673e09cac835b6548
omercsx <omercagris@gmail.com>
2024-10-16 08:33:06 -0400

    fonts and colors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
+
   root: true,
-  extends: '@react-native',
+
+  rules: {
+
+    // suppress errors for missing 'import React' in files
+
+    'react/react-in-jsx-scope': 'off',
+
+    // allow jsx syntax in js files (for next.js project)
+
+    'react/jsx-filename-extension': [
+
+      1,
+
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+
+    ],
+
+  },
+
+  extends: '@react-native-community',
+
 };
