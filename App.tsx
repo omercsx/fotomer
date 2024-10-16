5af135d3560852d8556ae56790c9b135bb6d5ec3
omercsx <omercagris@gmail.com>
2024-10-16 08:38:32 -0400

    react icons

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import {View, Text} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
     <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-      <Text>Hello World</Text>
+      <AntDesign name="home" size={24} color="black" />
     </View>
   );
 };
